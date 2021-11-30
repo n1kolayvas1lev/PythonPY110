c01100ad28ea5ca7c29ddaf602fe6ef41c82162d
n1kolayvas1lev <ckb01@ya.ru>
2021-11-30 21:12:35 +0300

    30.11.2021

--- Занятие4/Лабораторные_задания/task1_1/main.py
+++ Занятие4/Лабораторные_задания/task1_1/main.py
@@ -2,13 +2,13 @@ import json
 import re
 
 BOOKS_FILE = "books.md"
-BOOK_REGEX = ...  # TODO записать ругулярное выражения для поиска книги
+BOOK_REGEX = r'####\s(?P<position>\d+)\.\s\[(?P<book>.+?)\]\((?P<book_url>.+?)\)\sby\s+(?P<author>.+?)\s+\((?P<recommended>\d+\.\d+%).+?\((?P<cover_url>.+?)\).+?\"(?P<description>.+?)\"'  #  записать ругулярное выражения для поиска книги
 
 
 def task():
     book_pattern = re.compile(BOOK_REGEX, re.DOTALL)  # флаг re.DOTALL описывает, что под символом точка может содержаться символ переноса строки
 
-    with open(BOOKS_FILE) as f:
+    with open(BOOKS_FILE, encoding="utf-8") as f:
         for book in book_pattern.finditer(f.read()):
             print(json.dumps(book.groupdict(), indent=4))
 
